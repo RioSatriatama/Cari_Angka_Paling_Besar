669ae181e41db90033a1c4e17fad43b6c9e6b421
RioSatriatama <82066488+RioSatriatama@users.noreply.github.com>
2023-10-30 16:05:53 +0700

    Add files via upload

---
+++ CariAngkaPalingBesar/CariAngkaPalingBesar.vb
@@ -0,0 +1,20 @@
+﻿Module CariAngkaPalingBesar
+    'Buka CariAngkaPalingBesar.vbproj
+    Sub Main()
+        Dim a As Long, b As Long, c As Long
+        a = InputBox("Masuk nilai ke-1:")
+        b = InputBox("Masuk nilai ke-2:")
+        c = InputBox("Masuk nilai ke-3:")
+        Debug.Print("Nilai a = " & a)
+        Debug.Print("Nilai b = " & b)
+        Debug.Print("Nilai c = " & c)
+        If (a > b) And (a > c) Then
+            Debug.Print("Nilai terbesar = " & a)
+        ElseIf b > c Then
+            Debug.Print("Nilai terbesar = " & b)
+        Else
+            Debug.Print("Nilai terbesar = " & c)
+        End If
+    End Sub
+
+End Module
